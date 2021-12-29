180bdf5d25bde41646b1a23e950237db2fcfe72f
Abdelrhman-Hosny <ahosny2792@gmail.com>
2021-12-29 19:56:42 +0200

    add classifier object

---
+++ low-level/src/facedetection/classifier.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+from feature import Feature, Rectangle
+
+class WeakClassifier:
+    def __init__(self, classifier_threshold, feature_idx, success_val, fail_val) -> None:
+        
+        self.classifier_threshold = classifier_threshold
+        self.feature_idx = feature_idx  
+        self.success_value = success_val
+        self.fail_value = fail_val
+    
+    def classify(self, feature_list : list[Feature], integral_image : np.ndarray) -> float:
+
+        feature = feature_list[self.feature_idx]
+        feature_val = feature.compute_feature(integral_image)
+        if feature_val > self.classifier_threshold:
+            return self.success_value
+        else:
+            return self.fail_value
