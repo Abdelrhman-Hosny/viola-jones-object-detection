8d9b3dfa3b3f5ea2a34fff5e3aba567328b7c95b
Abdelrhman-Hosny <ahosny2792@gmail.com>
2021-12-29 20:42:54 +0200

    fix typos

--- low-level/src/facedetection/stage.py
+++ low-level/src/facedetection/stage.py
@@ -4,14 +4,14 @@ from classifier import WeakClassifier
 
 
 class Stage:
-    def __init__(self, stage_Threshold: int, classfier_list : list[WeakClassifier]) -> None:
-        self.stage_Threshold = stage_Threshold
-        self.classfier_list = classfier_list
+    def __init__(self, stage_threshold: int, classifier_list : list[WeakClassifier]) -> None:
+        self.stage_threshold = stage_threshold
+        self.classifier_list = classifier_list
         self.current_val = 0
     
     def check_stage(self, feature_list : list[Feature],  integral_image : np.ndarray) -> bool:
-        for classifer in self.classfier_list:
-            self.current_val += classifer.classify(feature_list,integral_image) 
+        for classifier in self.classifier_list:
+            self.current_val += classifier.classify(feature_list,integral_image) 
             if(self.current_val < self.stage_Threshold):
                 return False
         return True
